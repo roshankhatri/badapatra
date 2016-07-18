f83656ed7595eec9eab9cb64767d02e606c0bf01
Roshan Khatri <roshank@nec.edu.np>
2016-07-18 13:28:17 +0545

    Initial Commit to BadaPatra Project Button Activity added to Project

--- app/src/main/java/app/roshankhatri/com/badapatra/MainActivity.java
+++ app/src/main/java/app/roshankhatri/com/badapatra/MainActivity.java
@@ -1,5 +1,6 @@
 package app.roshankhatri.com.badapatra;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button button1;
+
+    public void init(){
+
+        button1=(Button) findViewById(R.id.button9);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent;
+                intent = new Intent(MainActivity.this,health.class);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        init();
     }
 
     @Override
